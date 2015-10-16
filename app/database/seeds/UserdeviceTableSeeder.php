52bef05f23faadd9650e31def713a7e8c6fa3a72
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2015-10-16 18:15:57 -0300

    add or and devices

--- app/database/seeds/UserdeviceTableSeeder.php
+++ app/database/seeds/UserdeviceTableSeeder.php
@@ -12,6 +12,16 @@ class UserdeviceTableSeeder extends Seeder {
 			'users_id' => '2',
 			'devices_id' => '1'
 		]);
+		foreach(range(2, 6) as $index) {
+			Userdevice::create([
+				'users_id' => '1',
+				'devices_id' => $index
+			]);
+			Userdevice::create([
+				'users_id' => '2',
+				'devices_id' => $index
+			]);
+		}
 
 	}
 
