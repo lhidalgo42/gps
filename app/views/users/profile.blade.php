0f8afe0cd88804ef9a02afa81080ddc1c3124c16
enzo9214 <enzo.9214@gmail.com>
2015-10-16 18:35:50 -0300

    PROFILE CON PATENTES FOREACH

--- app/views/users/profile.blade.php
+++ app/views/users/profile.blade.php
@@ -13,25 +13,30 @@
             <div class="row">
                 <div class="col-md-6 col-sm-12">
                     <h4 class="page-header">Datos del Usuario</h4>
+
                     <form>
                         <div class="form-group">
                             <label for="name">Nombre:</label>
-                            <input type="text" class="form-control" id="name" placeholder="Tu nombre" value="{{$user->name}}">
+                            <input type="text" class="form-control" id="name" placeholder="Tu nombre"
+                                   value="{{$user->name}}">
                         </div>
 
                         <div class="form-group">
                             <label for="rut">Rut:</label>
-                            <input type="text" class="form-control" id="rut" placeholder="Ejemplo xx.xxx.xxx-x" value="{{$user->rut}}">
+                            <input type="text" class="form-control" id="rut" placeholder="Ejemplo xx.xxx.xxx-x"
+                                   value="{{$user->rut}}">
                         </div>
 
                         <div class="form-group">
                             <label for="email">E-mail:</label>
-                            <input type="email" class="form-control" id="email" placeholder="Email" value="{{$user->email}}">
+                            <input type="email" class="form-control" id="email" placeholder="Email"
+                                   value="{{$user->email}}">
                         </div>
 
                         <div class="form-group">
                             <label for="phone">Rut:</label>
-                            <input type="text" class="form-control" id="phone" placeholder="Ejemplo x-xxxxxxx" value="{{$user->phone}}">
+                            <input type="text" class="form-control" id="phone" placeholder="Ejemplo x-xxxxxxx"
+                                   value="{{$user->phone}}">
                         </div>
 
                         <button type="submit" class="btn btn-default">Actualizar</button>
@@ -39,22 +44,23 @@
 
                 </div>
 
+
                 <div class="col-md-6 col-sm-12">
                     <h4 class="page-header">Subscripción</h4>
-                    <div class="input-group input-group-md" style="padding-top: 24px;">
-                        <span class="input-group-addon" id="sizing-addon1">PATENTE</span>
-                        <input type="text" class="form-control" value="oli">
-                    </div>
+                    <p style="font-weight: bold; margin-bottom:5px;">Valida hasta:</p>
+                    @foreach($devices as $device)
+                        <div class="input-group input-group-md"style="padding-bottom: 40px">
+                            <span class="input-group-addon" id="sizing-addon1">{{strtoupper($device->plate)}}</span>
+                            <input type="text" class="form-control" value="">
+                        </div>
+                    @endforeach
                 </div>
 
-
             </div>
+            <!-- /#page-wrapper -->
+
 
         </div>
-        <!-- /#page-wrapper -->
-
-
-    </div>
-    <!-- /#wrapper -->
+        <!-- /#wrapper -->
 
 @stop
